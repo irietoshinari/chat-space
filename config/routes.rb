255f0f15d8fecd2891e4a43a263a51a383a18e22
irietoshinari <60064033+irietoshinari@users.noreply.github.com>
2020-02-05 20:56:48 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
+=======
+  devise_for :users
+  get 'messages/index'
+
+>>>>>>> Stashed changes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
